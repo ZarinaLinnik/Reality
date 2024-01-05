b14975b9f5dff1e9a72c9fa2b0c6cd8722b3424d
ZarinaLinnik <zarinaelan2005@gmail.com>
2024-01-06 00:21:30 +0300

    Parameter IMG

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.conf import settings
 from django.shortcuts import render, redirect
 from .models import Parameter1WhoAreYou, ParameterIMGMyPhoto
 from .forms import AddParameter1WhoAreYou, AddParameterIMGMyPhoto
@@ -32,7 +33,6 @@ def creature(request):
 @login_required
 def add_parameter1_who_are_you(request):
     user = User.objects.get(username=request.user)
-
     if Parameter1WhoAreYou.objects.filter(user_id=user.id).exists() and request.method == 'GET':
         model = Parameter1WhoAreYou.objects.get(user_id=user.id)
         return render(request, 'show_content.html', {'model':model})
@@ -50,13 +50,16 @@ def add_parameter1_who_are_you(request):
 
 @login_required
 def my_photo(request):
+    user = User.objects.get(username=request.user)
+    if ParameterIMGMyPhoto.objects.filter(user_id=user.id).exists() and request.method == 'GET':
+        media_url = settings.MEDIA_URL
+        model = ParameterIMGMyPhoto.objects.get(user_id=user.id)
+        return render(request, 'show_my_photo.html', {'image':model.image, 'media':media_url})
+    
     if request.method == 'POST':
         form = AddParameterIMGMyPhoto(request.POST, request.FILES)
         if form.is_valid():
-            taken_info = form.save(commit=False)
-            taken_info.date_time = datetime.datetime.now()
-            taken_info.user = User.objects.get(username=request.user)
-            taken_info.save()         
+            form.save()     
     else:
         form = AddParameterIMGMyPhoto()
     return render(request, 'my_photo.html', {'form': form})
