f5657f1fd73d3abddae219983c2e64f2f6b2474c
ZarinaLinnik <zarinaelan2005@gmail.com>
2024-01-18 01:17:59 +0300

    some moments

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from .models import Parameter1WhoAreYou, ParameterIMGMyPhoto
 
 # Register your models here.
-from .models import Parameter1WhoAreYou
-
 admin.site.register(Parameter1WhoAreYou)
+admin.site.register(ParameterIMGMyPhoto)
